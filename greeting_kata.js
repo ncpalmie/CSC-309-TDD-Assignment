6af8e81a49acb14327f8bcea451245f29259457d
ncpalmie <ncpalmie@calpoly.edu>
2020-04-08 16:50:56 -0700

    Refactored code and completed Requirement 4

--- greeting_kata.js
+++ greeting_kata.js
@@ -1,13 +1,23 @@
 function greet(name) {
     retString = 'Hello, '
 
-    if (name == null) {
-        name = 'my friend';
+    if (!Array.isArray(name)) {
+        tmp_name = name;
+        name = [];
+        if (tmp_name == null)
+            name.push('my friend');
+        else
+            name.push(tmp_name);
     }
 
-    retString += name;
+    if (name.length == 1) {
+        retString += name[0];
+    }
+    else if (name.length == 2) {
+        retString += name[0] + ' and ' + name[1];
+    }
 
-    if (isNameUppercase(name)) {
+    if (isNameUppercase(name[0])) {
         retString = retString.toUpperCase();
         retString += '!';
     }

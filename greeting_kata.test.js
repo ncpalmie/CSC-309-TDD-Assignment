8d8d39dd5c27737944440401936963ba0e2d848c
ncpalmie <ncpalmie@calpoly.edu>
2020-04-08 17:22:01 -0700

    Requirement 7 test written

--- greeting_kata.test.js
+++ greeting_kata.test.js
@@ -1,26 +1,29 @@
-const greet = require('./greeting_kata')
+const greet = require('./greeting_kata');
 
 test('Name Greeting', () => {
     expect(greet('Bob')).toBe('Hello, Bob.');
 });
 
 test('Null Greeting', () => {
-    expect(greet(null)).toBe('Hello, my friend.')
+    expect(greet(null)).toBe('Hello, my friend.');
 });
 
 test('Shout Greeting', () => {
-    expect(greet('JERRY')).toBe('HELLO JERRY!')
+    expect(greet('JERRY')).toBe('HELLO JERRY!');
 });
 
 test('Two Name Greeting', () => {
-    expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.')
+    expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.');
 });
 
 test('Multi-Name Greeting', () => {
-    expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe('Hello, Amy, Brian, and Charlotte.')
+    expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe('Hello, Amy, Brian, and Charlotte.');
 });
 
 test('Multi-Name Shout Greeting', () => {
-    expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!')
+    expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!');
 });
 
+test('Comma Name Greeting', () => {
+    expect(greet(['Bob', 'Charlie, Dianne'])).toBe('Hello, Bob, Charlie, and Dianne');
+});
